7d7f7c3a873b284b2039551776d215663076fa47
kz-oita <taoi.kizuka@gmail.com>
2024-09-17 08:30:05 +0900

    use getter manday

--- Desktop/ts_training/src/app.ts
+++ Desktop/ts_training/src/app.ts
@@ -130,6 +130,14 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
   private project: Project;
 
+  get manday() {
+    if(this.project.manday < 20){
+      return this.project.manday.toString() + '人日'
+    } else {
+      return (this.project.manday / 20).toString() + '人月'
+    }
+  }
+
   constructor(hostId: string, project: Project){
     super('single-project', hostId, false, project.id);
     this.project = project;
@@ -142,7 +150,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
 
   renderContent(){
     this.element.querySelector('h2')!.textContent = this.project.title;
-    this.element.querySelector('h3')!.textContent = this.project.manday.toString();
+    this.element.querySelector('h3')!.textContent = this.manday;
     this.element.querySelector('p')!.textContent = this.project.description;
   }
 
@@ -151,7 +159,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
 //ProjectList class
 class ProjectList extends Component<HTMLDivElement, HTMLElement>{
   assignedProjects: Project[];
-  
+
   constructor(private type: 'active' | 'finished') {
     super('project-list', 'app', false, `${type}-projects`);
     this.assignedProjects = [];
